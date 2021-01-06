1e7797eb53180697cc31034762e4b836fbd29a9e
BJ Clark <bjclark13@outlook.com>
2021-01-06 14:28:51 -0500

    Add animal crossings app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VillagersComponent } from './villagers/villagers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VillagersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/villagers/villagers.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from "@angular/core";
+
+interface Villager {
+  id: number;
+  name: string;
+  personality: string;
+  birthday: Date;
+  gender: String;
+  "catch-phrase": string;
+  image_uri: string;
+  "bubble-color": string;
+  "text-color": string;
+}
+
+@Component({
+  selector: "app-villagers",
+  templateUrl: "./villagers.component.html",
+  styleUrls: ["./villagers.component.css"],
+})
+export class VillagersComponent implements OnInit {
+  public villagers: Villager[];
+
+  constructor() {
+    this.villagers = [
+      {
+        id: 12,
+        name: "Nate",
+        personality: "Lazy",
+        birthday: new Date(2020, 8, 16),
+        gender: "Male",
+        "catch-phrase": "yawwwn",
+        image_uri: "https://acnhapi.com/v1/images/villagers/12",
+        "bubble-color": "#e8b010",
+        "text-color": "#fffce9",
+      },
+      {
+        id: 200,
+        name: "Bertha",
+        personality: "Normal",
+        birthday: new Date(2020, 4, 25),
+        gender: "Female",
+        "catch-phrase": "bloop",
+        image_uri: "https://acnhapi.com/v1/images/villagers/200",
+        "bubble-color": "#00d1bd",
+        "text-color": "#fffce9",
+      },
+      {
+        id: 20,
+        name: "Beardo",
+        personality: "Smug",
+        birthday: new Date(2020, 12, 20),
+        gender: "Male",
+        "catch-phrase": "whiskers",
+        image_uri: "https://acnhapi.com/v1/images/villagers/20",
+        "bubble-color": "#3fd8e0",
+        "text-color": "#fffce9",
+      },
+    ];
+  }
+
+  ngOnInit() {}
+}
